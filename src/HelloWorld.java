ea1e5fb0fd8130b2334dd9bdca93cc5556f207b4
Lukasz <szamikmic@o2.pl>
2024-04-24 21:06:45 +0200

    add to ignore

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,42 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj pierwsza liczbe");
-                int pierwszaLiczba = scanner.nextInt();
-        System.out.println("podaj druga liczbe");
-                int drugaLiczba = scanner.nextInt();
-                int result = pierwszaLiczba + drugaLiczba;
-        System.out.println("Dodawanie " + result );
+            System.out.println("Podaj liczbę");
+
+            int cyfra = scanner.nextInt();
+
+            if (cyfra > 0) {
+
+                System.out.println("Czy jest większa od 100? Y/N");
+                String confirm = scanner.next();
+
+                if(Objects.equals(confirm, "Y") && (cyfra > 100)) {
+                    System.out.println("Niech zgadnę.. Twoja liczba to.." + cyfra + " ! :)");                        // kiedy liczba > od 100 i odp "Y" - prawda
+                } else {
+                    System.out.println("nie oszukuj!");                 // kiedy liczba < 100 i odp "Y" - nie prawda
+                    System.out.println("Jest czy nie jest większa od 100? Y/N");
+                    String drugaSzansa = scanner.next();
+                    if(Objects.equals(drugaSzansa, "Y") && (cyfra > 100)) {
+                        System.out.println("no..teraz mówisz prawdę :)");
+                        System.out.println("Niech zgadnę.. Twoja liczba to.." + cyfra + " ! :)");
+                    } else {
+                        System.out.println("Koniec zabawy - nie bawię sie tak!");
+                    }
+                }
+            }
+
+
+
+
+
+
+
     }
+
 
 }
