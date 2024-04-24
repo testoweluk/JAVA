a54c0f43b078425b19a4f6cca8cb74050b325bfb
Lukasz <szamikmic@o2.pl>
2024-04-24 17:11:41 +0200

    new changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,9 +4,12 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-                String name = scanner.nextLine();
-
-        System.out.println("hello " + name + " !");
+        System.out.println("podaj pierwsza liczbe");
+                int pierwszaLiczba = scanner.nextInt();
+        System.out.println("podaj druga liczbe");
+                int drugaLiczba = scanner.nextInt();
+                int result = pierwszaLiczba + drugaLiczba;
+        System.out.println("Dodawanie " + result );
     }
 
 }
