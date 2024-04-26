bacf956092f7520f2a37a4fc0fc6c30096f0aa05
Lukasz <szamikmic@o2.pl>
2024-04-26 16:52:43 +0200

    add to ignore

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    public String name;
+    public int id_index;
+    public String sex;
+
+    public void przedstawSie() {
+        System.out.println("cześć" + name);
+    }
+
+    public void podajNumerIndeksu() {
+        System.out.println("Twój nr indeksu to" + id_index);
+    }
+
+    public void sex() {
+        System.out.println("Jesteś" + sex);
+    }
+
+}
---
+++ src/StudentSolve.java
@@ -0,0 +1,29 @@
+public class StudentSolve {
+
+    public static void main(String[] args) {
+
+        Student marek = new Student();
+        marek.name = "Marek K";
+        marek.id_index = 26226;
+        marek.sex = "M";
+
+        Student tomek = new Student();
+        tomek.name = "Tomek O";
+        tomek.id_index = 58226;
+        tomek.sex = "M";
+
+        Student kasia = new Student();
+        kasia.name = "Kasia K";
+        kasia.id_index = 49586;
+        kasia.sex = "F";
+
+        Students[] Student = new Students[3];
+        Students[0] = marek;
+        Students[1] = tomek;
+        Students[2] = kasia;
+
+        for (int i = 0; i < Students.length; i++) {
+        Student.
+        }
+    }
+}
